339907faf16a20c9b54f43d9ef23988babd583bd
Rayhan5513 <58135041+Rayhan5513@users.noreply.github.com>
2025-01-21 14:55:58 +0600

    navbar created

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  // Add the Tailwind CSS file
+  css: ['@/assets/css/tailwind.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  compatibilityDate: '2025-01-21',
+});
