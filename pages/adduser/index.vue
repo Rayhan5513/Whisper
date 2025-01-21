3087ae045cfbdbf0bc3ba9a738b7d91939dfd269
ronssrony <ronssrony@gmail.com>
2025-01-21 17:36:26 +0600

    fix index

--- pages/adduser/index.vue
+++ pages/adduser/index.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="flex justify-center gap-32 pt-20 items-center ">
-    <UiAddform/>
+  <div class="flex flex-col justify-center gap-4 pt-20 items-center ">
+   
+    <div class="card flex flex-col  justify-center">
+        <Button label="User +" @click="visible = true"  />
+        <Dialog v-model:visible="visible" modal header="Create User" :style="{ width: '25rem' }">
+             <UiAddform/>
+        </Dialog>
+    </div>
    <div class="flex flex-col text-xl gap-4 ">
     <h1 class="text-3xl font-semibold">Users</h1>
      <div v-for="user in userStore.userList" class="flex flex-col">
@@ -13,7 +19,9 @@
 <script lang="ts" setup>
 import { UiAddform } from '#components';
 import { useMyUserStore } from '~/store/userStore';
-
+import { Button } from 'primevue';
+import {Dialog} from 'primevue';
+const visible = ref(false);``
 
 const userStore = useMyUserStore(); 
 
