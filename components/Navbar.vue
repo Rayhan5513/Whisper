a447129e5fb30478cee6bb2ee2f1632e734b4981
ronssrony <ronssrony@gmail.com>
2025-01-22 14:14:07 +0600

    fix the gap of navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="shadow-md px-4 py-2 rounded-md" >
      <header>
-      <nav class="flex justify-between items-center">
+      <nav class="flex justify-between items-center ">
         <NuxtLink to="/">
            <h1 class="font-bebas text-5xl tracking-wider">Whisper</h1>
         </NuxtLink>
-        <ul class="flex items-center gap-2 ">
+        <ul class="flex items-center gap-4 ">
           <InputText class="w-32 max-sm:hidden h-9 text-sm " type="text" placeholder="Search"  />
           <NuxtLink to="/users"><Button class="w-12 h-9">Users</Button></NuxtLink>
           <NuxtLink to="/message"><Button class="w-12 h-9 ">Chat</Button></NuxtLink>
