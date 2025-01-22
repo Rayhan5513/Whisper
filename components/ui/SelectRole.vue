4ca4381ff51b5c65dd6d05a159218ba7de425f42
ronssrony <ronssrony@gmail.com>
2025-01-22 10:32:05 +0600

    add radio button for gender and role

---
+++ components/ui/SelectRole.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div class="card flex justify-center">
+        <SelectButton v-model="value" :options="options" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { SelectButton } from 'primevue';
+const value = ref('User');
+const emit = defineEmits(['selectedRole'])
+const options = ref(['User', 'Admin']);
+
+watch(value , ()=>{
+  emit('selectedRole',value.value)
+})
+</script>
--- components/ui/addform.vue
+++ components/ui/addform.vue
@@ -3,10 +3,10 @@
     <div class="card flex justify-center">
 
         <Form @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-56">
-            <div class="flex flex-col gap-4">
-             <InputText v-model="inputName" name="username" type="text" placeholder="Username" fluid />      
-             <InputText v-model="inputRole" name="role" type="text" placeholder="Role" fluid /> 
-            <InputText v-model="inputGender" name="gender" type="text" placeholder="Gender" fluid />    
+            <div class="flex flex-col gap-5">
+                <UiSelectRole  @selectedRole="handeRole"/>
+                <InputText v-model="inputName" name="username" type="text" placeholder="Username" fluid />      
+             <UiSelectGender @selectedGender="handleGender"/>
             </div>
             <Button type="submit" severity="secondary" label="Submit" />
         </Form>
@@ -18,11 +18,19 @@ import { Form } from '@primevue/forms';
 import { InputText } from 'primevue';
 import {Button} from 'primevue'
 import { useMyUserStore } from '~/store/userStore';
-const inputName = ref<string>('')
-const inputRole = ref<string>('')
-const inputGender = ref<string>('')
 const emit = defineEmits(['submituser'])
+const inputName = ref<string>('')
+const inputRole = ref<string>('User')
+const inputGender = ref<string>('Male')
 const userStore = useMyUserStore(); 
+
+const handeRole = (value:string)=>{
+    inputRole.value = value
+}
+const handleGender = (value:string)=>{
+    inputGender.value = value ;
+}
+
 const onFormSubmit = () => {
    const user =  {
             name:inputName.value ,
