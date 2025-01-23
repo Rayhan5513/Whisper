bab2bfbaca5110a035c91cc7b5bb3f4abc8ca7c5
Rayhan5513 <58135041+Rayhan5513@users.noreply.github.com>
2025-01-23 16:24:13 +0600

    image chatting

--- components/Message.vue
+++ components/Message.vue
@@ -1,8 +1,9 @@
 <template>
-  <div
+  <div v-if="message.message.length>0"
        class=" messagecart bg-green-300 text-black max-w-fit self-end px-2 py-1 rounded-tr-xl rounded-bl-xl flex items-center text-lg justify-center  ">
     
     <span>{{message.message}}</span>
+  
   </div>
 </template>
 
